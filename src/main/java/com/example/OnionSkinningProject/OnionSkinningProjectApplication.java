f96d62f27ab8cc6612fc765ba8a432a847740011
Emanue Peter <emanuel23.peter@gmail.com>
2025-12-05 15:46:11 +0100

    first commit

---
+++ src/main/java/com/example/OnionSkinningProject/OnionSkinningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.OnionSkinningProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnionSkinningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnionSkinningProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/OnionSkinningProject/OnionSkinningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.OnionSkinningProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnionSkinningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
